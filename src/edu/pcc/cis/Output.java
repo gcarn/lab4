62a4afa072818fd5d269c6b1bc0349f029cb1602
pineTreeWeb <pinetreewebd@gmail.com>
2017-10-24 21:51:20 -0700

    Added weather report

--- src/edu/pcc/cis/Output.java
+++ src/edu/pcc/cis/Output.java
@@ -1,7 +1,10 @@
+//Author: Glen Carney
+
 package edu.pcc.cis;
 
 public class Output {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Nice weather!!");
     }
 }
